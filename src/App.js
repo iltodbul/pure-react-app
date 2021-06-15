cfacf0c99a04b97bd9584f0c765c8b01d014e1ad
Iliya Todorov <53480117+iltodbul@users.noreply.github.com>
2021-06-15 11:35:25 +0300

    App structure

--- src/App.js
+++ src/App.js
@@ -1,9 +1,20 @@
+import React, { Component } from 'react';
 import './App.css';
 
-function App() {
-  return (
-    <h1 className='App'>App component</h1>
-  );
+class App extends Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      winningNumber: 0,
+      tickets: [],
+      remainingTicket: 5,
+      finished: false,
+    };
+  }
+  render() {
+    return <div className="App"></div>;
+  }
 }
 
 export default App;
