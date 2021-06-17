d98ec5e08daba8bfb7182534efa7a086a0fba01e
Iliya Todorov <53480117+iltodbul@users.noreply.github.com>
2021-06-17 16:19:19 +0300

    class to functional component

--- src/Components/AppFooter.js
+++ src/Components/AppFooter.js
@@ -1,16 +1,14 @@
-import React, { Component } from 'react';
+import React from 'react';
 import { Layout } from 'antd';
 
 const { Footer } = Layout;
 
-class AppFooter extends Component {
-  render() {
-    return (
-      <Footer style={{ textAlign: 'center' }}>
-        На Божков Лотарията ©2021 Created with React - Ant Design - React Router
-      </Footer>
-    );
-  }
+function AppFooter() {
+  return (
+    <Footer style={{ textAlign: 'center' }}>
+      На Божков Лотарията ©2021 Created with React - Ant Design - React Router
+    </Footer>
+  );
 }
 
 export default AppFooter;
--- src/Components/LotteryTicket.js
+++ src/Components/LotteryTicket.js
@@ -1,34 +1,32 @@
-import React, { Component } from 'react';
+import React from 'react';
 import { Button, Card, Typography } from 'antd';
 
 const { Title } = Typography;
 
-class LotteryTicket extends Component {
-  render() {
-    const { color, number, actions, index } = this.props;
-    return (
-      <Card
-        style={{
-          backgroundColor: color,
-          marginBottom: 25,
+function LotteryTicket() {
+  const { color, number, actions, index } = this.props;
+  return (
+    <Card
+      style={{
+        backgroundColor: color,
+        marginBottom: 25,
+      }}
+      size="small"
+      title="Лотариен билет с номер"
+    >
+      <b>
+        <Title level={4}>{number}</Title>
+      </b>
+      <Button
+        onClick={() => {
+          actions.removeTicket(index);
         }}
-        size="small"
-        title="Лотариен билет с номер"
+        block
       >
-        <b>
-          <Title level={4}>{number}</Title>
-        </b>
-        <Button
-          onClick={() => {
-            actions.removeTicket(index);
-          }}
-          block
-        >
-          X
-        </Button>
-      </Card>
-    );
-  }
+        X
+      </Button>
+    </Card>
+  );
 }
 
 export default LotteryTicket;
