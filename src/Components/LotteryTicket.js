6ef182f0a5ac48a3ce768dc2f5fced9165031c1e
Iliya Todorov <53480117+iltodbul@users.noreply.github.com>
2021-09-28 11:17:58 +0300

    Update LotteryTicket.js

--- src/Components/LotteryTicket.js
+++ src/Components/LotteryTicket.js
@@ -1,32 +1,34 @@
-import React from 'react';
+import React, { Component } from 'react';
 import { Button, Card, Typography } from 'antd';
 
 const { Title } = Typography;
 
-function LotteryTicket() {
-  const { color, number, actions, index } = this.props;
-  return (
-    <Card
-      style={{
-        backgroundColor: color,
-        marginBottom: 25,
-      }}
-      size="small"
-      title="Лотариен билет с номер"
-    >
-      <b>
-        <Title level={4}>{number}</Title>
-      </b>
-      <Button
-        onClick={() => {
-          actions.removeTicket(index);
+class LotteryTicket extends Component {
+  render() {
+    const { color, number, actions, index } = this.props;
+    return (
+      <Card
+        style={{
+          backgroundColor: color,
+          marginBottom: 25,
         }}
-        block
+        size="small"
+        title="Лотариен билет с номер"
       >
-        X
-      </Button>
-    </Card>
-  );
+        <b>
+          <Title level={4}>{number}</Title>
+        </b>
+        <Button
+          onClick={() => {
+            actions.removeTicket(index);
+          }}
+          block
+        >
+          X
+        </Button>
+      </Card>
+    );
+  }
 }
 
 export default LotteryTicket;
